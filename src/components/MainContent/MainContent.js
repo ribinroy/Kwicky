5b2d08bc3656c52e987da954e19e6667abc433a2
Ribin <ribinroy4@gmail.com>
2020-10-18 22:54:04 +0530

    Warning fixed

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState } from 'react';
 import Moment from 'react-moment';
 import './MainContent.scss';
 import AdminContext from './../../store/Context';
